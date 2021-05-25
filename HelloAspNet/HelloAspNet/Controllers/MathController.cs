666e6e8bf88c6d4bda492b03464d6598b10d02c6
Rainer Stropek <rainer@software-architects.at>
2021-05-25 13:50:10 +0200

    Dependency Injection

---
+++ HelloAspNet/HelloAspNet/Controllers/MathController.cs
@@ -0,0 +1,38 @@
+﻿using MathematicsHelper;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloAspNet.Controllers
+{
+    //public interface ICalculator
+    //{
+    //    int Add(int x, int y);
+    //}
+
+    //public class Calculator : ICalculator
+    //{
+    //    public int Add(int x, int y) => x + y;
+    //}
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class MathController : ControllerBase
+    {
+        private readonly ICalculator calc;
+
+        public MathController(ICalculator calc)
+        {
+            this.calc = calc;
+        }
+
+        // https://localhost:5001/api/math?x=21&y=21
+
+        [HttpGet]
+        public int Add([FromQuery] int x, [FromQuery] int y)
+            => calc.Add(x, y);
+    }
+}
---
+++ HelloAspNet/MathematicsHelper/Calculator.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace MathematicsHelper
+{
+    public interface ICalculator
+    {
+        int Add(int x, int y);
+    }
+
+    public class Calculator : ICalculator
+    {
+        public int Add(int x, int y) => x + y;
+    }
+
+    public static class AspNetCoreCalculatorExtensions
+    {
+        public static void AddMathematicsHelpers(this IServiceCollection services)
+        {
+            services.AddSingleton<ICalculator, Calculator>();
+            // ...
+        }
+    }
+}
